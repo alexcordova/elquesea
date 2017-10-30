9f3181008da2e908c9134cd8b9aeb6b2cbddbd74
alexcordova <aleex.cordova@hotmail.com>
2017-10-30 11:14:25 -0600

    mi primer commit

---
+++ index.php
@@ -0,0 +1,141 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+    <!-- Custom styles for this template -->
+    <link href="css/x.css" rel="stylesheet">
+</head>
+
+<body>
+
+<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="https://www.google.com">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.pinterest.com.mx/">Pinterest</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Paginas</a>
+                <div class="dropdown-menu" aria-labelledby="dropdown01">
+                    <a class="dropdown-item" href="https://www.facebook.com/alejandro.cordova.5243">Facebook</a>
+                    <a class="dropdown-item" href="https://twitter.com/alejandhro">Twitter</a>
+                    <a class="dropdown-item" href="https://www.instagram.com/alex.cordova/?hl=es">Instagram</a>
+                </div>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+
+<main role="main" class="container">
+
+    <div class="starter-template">
+        <h1>Jesus Alejandro Cordova Almazan</h1>
+        <p class="lead">“Hay momentos en los que un hombre tiene que luchar y hay momentos en los que debe aceptar que ha perdido, su destino, que el barco ha zarpado, que solo un iluso seguiría insistiendo. Pero lo cierto es que siempre he sido un iluso.” Big Fish</p>
+    </div>
+    <div class="row">
+        <div class="col-sm-4">
+            <img src="img/yo.jpg" width="250" height="250" class="rounded-circle border border-danger">
+        </div>
+        <div class="col-sm-4">
+            <h1>Abilidades</h1>
+            <h2>
+            <li>Programador</li>
+            <li>Diseñador</li>
+            </h2>
+        </div>
+        <div class="col-sm-4">
+            <h1>Links</h1>
+            <h2>
+                <h4><li><button type="button" class="btn btn-danger"><a href="https://www.facebook.com/alejandro.cordova.5243"><h3>Facebook</h3></a></button></li></h4>
+                <h4><li><button type="button" class="btn btn-danger"><a href="https://twitter.com/alejandhro"><h3>Twitter</h3></a></button></li></h4>
+                <h4><li><button type="button" class="btn btn-danger"><a href="https://www.instagram.com/alex.cordova/?hl=es"><h3>Instagram</h3></a></button></li></h4>
+            </h2>
+        </div>
+
+        <div class="container">
+    <div class="row">
+        <div class="col-sm-6">
+            <table class="table">
+                <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Lunes</th>
+                    <th scope="col">Martes</th>
+                    <th scope="col">Miercoles</th>
+                    <th scope="col">Jueves</th>
+                    <th scope="col">Viernes</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">8:00 - 10:00</th>
+                    <td>Composicion</td>
+                    <td>Diseño</td>
+                    <td>Diseño</td>
+                    <td>Composicion</td>
+                    <td>Progra</td>
+                </tr>
+                <tr>
+                    <th scope="row">10:00 - 12:00</th>
+                    <td>Sistemas</td>
+                    <td>Proyecto</td>
+                    <td>Progra</td>
+                    <td>Sistemas</td>
+                    <td>Proyecto</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="col-sm-6">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <img src="img/yo.jpg" width="150" height="150" class="rounded border border-danger">
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="img/yo.jpg" width="150" height="150" class="rounded-circle border border-danger">
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="img/yo.jpg" width="150" height="150" class="rounded border border-danger">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+        </div>
+</main><!-- /.container -->
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+
+</body>
+</html>
